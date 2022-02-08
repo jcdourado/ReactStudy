7b274f8a5a1e2568e2749686feb75403f48d2519
Julio Cezar Dourado <juliodourado@ext.cvccorp.com.br>
2022-02-08 13:34:23 -0300

    Add Storybook Addons: docs, viewports and knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,10 +6,18 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-viewport",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
+    "@storybook/addon-knobs",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
