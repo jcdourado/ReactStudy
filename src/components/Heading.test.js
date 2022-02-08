b4d13540a71baf3866f1781dcbd60d236682f77d
Julio Cezar Dourado <juliodourado@ext.cvccorp.com.br>
2022-02-08 13:36:46 -0300

    Add Heading Component

---
+++ src/components/Heading.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { render } from "@testing-library/react";
+
+import Heading from "./Heading";
+
+test("match snapshot", () => {
+  const { asFragment } = render(<Heading>Title</Heading>);
+
+  expect(asFragment()).toMatchSnapshot();
+});
