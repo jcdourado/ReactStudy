e8fa691dbf9a03ead65f96ca0e6fa417dbdc75aa
Julio Cezar Dourado <juliodourado@ext.cvccorp.com.br>
2022-02-08 14:05:51 -0300

    Fix font-family

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 
+import { Helmet } from "react-helmet";
+import React from "react";
+
 import { BreakpointSizes, breakAt } from "./Breakpoints";
 
 const colorYellow = "#ffc107";
@@ -83,4 +86,16 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-export default GlobalStyle;
+const GlobalStyleComposed = () => (
+  <>
+    <GlobalStyle />
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+  </>
+);
+
+export default GlobalStyleComposed;
